97de9771158b2a26e8b9645450e9da6e04823cc9
AndAccioly <andreacciolyl@hotmail.com>
2022-04-10 15:53:59 -0300

    Inicital project back end

---
+++ src/main/java/amanda/amanda/AmandaApplication.java
@@ -0,0 +1,13 @@
+package amanda.amanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmandaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amanda/amanda/AmandaApplicationTests.java
@@ -0,0 +1,13 @@
+package amanda.amanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
